ec7cff3190d27520cea6baa7c15fc03ca558cd82
Syed Hussain Ather <shussainather@gmail.com>
2021-05-25 19:09:42 -0400

    test and print

--- scratch.py
+++ scratch.py
@@ -6,3 +6,27 @@ Use this script for playing around with Networkx.
 """
 
 G = nx.Graph()
+G.add_weighted_edges_from([(1, 2, 0.5), (3, 1, 0.75)])
+
+d = {0: {1: 0}, 1: {2: 5, 3: 1}, 2: {2: 0}, 3: {3: 0}}
+
+def tpsol(d, s=0, p=[], f=0):
+    """
+    Find the Trolley Problem solution given the utilitarian argument described.
+    Arguments:
+        d : A dictionary with a key to describe the neighbours 
+            and value as a number of people trapped to each track
+        s :   
+    """
+    return f in p and s or min(tpsol(d,s+d[f][t],p+[f],t) for t in d[f])
+
+def tpsolnx(G, s=0, p=[], f=0):
+    """
+    
+    """
+    return
+
+for n, nbrs in G.adj.items():
+   for nbr, eattr in nbrs.items():
+       wt = eattr['weight']
+       if wt < 0.5: print(f"({n}, {nbr}, {wt:.3})") 
